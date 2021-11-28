8f860c747a0636e7930e0f5ec679bfddb5e0b20c
Ignacio Aristo <ignacioaristo@gmail.com>
2021-11-28 18:32:59 -0300

    MS-22: create list all sessions function

--- src/Components/Sessions/index.js
+++ src/Components/Sessions/index.js
@@ -1,9 +1,47 @@
 import styles from './sessions.module.css';
+import { useState, useEffect } from 'react';
 
 function Sessions() {
+  const [sessions, saveSessions] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/api/sessions/`)
+    .then((response) => response.json())
+    .then((response) => {
+      saveSessions(response.Sessions);
+    });
+  }, []);
+
   return (
     <section className={styles.container}>
-      <h2>Sessions</h2>
+      <h2>Clients</h2>
+      <table>
+        <thead>
+          <th>Postulant Id</th>
+          <th>Counselor Id</th>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Accomplished</th>
+        </thead>
+        <tbody>
+          {sessions.map((session) => {
+            return (
+              <tr key={session._id}>
+                <td>{session.postulantId}</td>
+                <td>{session.counselorId}</td>
+                <td>{session.date}</td>
+                <td>{session.time}</td>
+                <td>{session.accomplished.toString()}</td>
+                <td>
+                  <button >
+                    X
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button className={styles.addButton}>ADD SESSION</button>
     </section>
   );
 }
