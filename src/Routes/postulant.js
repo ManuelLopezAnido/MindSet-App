acbd2227c650f319a6f8ab74c692233e8731f697
Ignacio Aristo <ignacioaristo@gmail.com>
2021-12-19 19:28:09 -0300

    MS-68: create route for sign up

--- src/Routes/postulant.js
+++ src/Routes/postulant.js
@@ -1,6 +1,7 @@
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import Home from 'Components/Postulant/Home';
 import PostulantLayout from 'Components/Layout/PostulantLayout';
+import SignUp from 'Components/Postulant/SignUp';
 
 const PostulantRoutes = () => {
   const { url } = useRouteMatch();
@@ -9,6 +10,7 @@ const PostulantRoutes = () => {
       <Switch>
         <Route path={`${url}/`} exact component={Home} />
         <Route path={`${url}/profile`} component={Home} />
+        <Route path={`${url}/signUp`} component={SignUp} />
       </Switch>
     </PostulantLayout>
   );
