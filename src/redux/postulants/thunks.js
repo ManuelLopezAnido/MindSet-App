5123ddbd79a2aa1f3e230f5911d78cc94f28bbbb
Micaela Quarleri <micaquarleri@gmail.com>
2021-12-22 07:42:43 -0300

    resolve the problem with the delete

--- src/redux/postulants/thunks.js
+++ src/redux/postulants/thunks.js
@@ -338,7 +338,7 @@ export const deletePostulant = (id) => (dispatch) => {
     }
   })
     .then((response) => {
-      if (response.status != 200) throw response;
+      if (response.status != 204) throw response;
       dispatch(deletePostulantFulfilled(id));
     })
     .catch((error) => dispatch(deletePostulantRejected(error.statusText)));
