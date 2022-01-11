2dbe702b028ccf7404da135d419a27cd79663503
Joaquin leimeter <leimeter.joaquin@gmail.com>
2022-01-10 21:58:45 -0300

    MS-97: created redux logic

--- src/redux/interviews/reducer.js
+++ src/redux/interviews/reducer.js
@@ -2,6 +2,15 @@ import {
   GET_INTERVIEWS_FETCHING,
   GET_INTERVIEWS_FULFILLED,
   GET_INTERVIEWS_REJECTED,
+  GET_ONE_INTERVIEW_FETCHING,
+  GET_ONE_INTERVIEW_FULFILLED,
+  GET_ONE_INTERVIEW_REJECTED,
+  UPDATE_INTERVIEW_FETCHING,
+  UPDATE_INTERVIEW_FULFILLED,
+  UPDATE_INTERVIEW_REJECTED,
+  ADD_INTERVIEW_FETCHING,
+  ADD_INTERVIEW_FULFILLED,
+  ADD_INTERVIEW_REJECTED,
   DELETE_INTERVIEW_FETCHING,
   DELETE_INTERVIEW_FULFILLED,
   DELETE_INTERVIEW_REJECTED,
@@ -36,6 +45,66 @@ const reducer = (state = initialState, action) => {
         isLoading: false,
         error: action.payload
       };
+
+    case GET_ONE_INTERVIEW_FETCHING: {
+      return {
+        ...state,
+        isLoading: true
+      };
+    }
+    case GET_ONE_INTERVIEW_FULFILLED: {
+      return {
+        ...state,
+        selected: action.payload,
+        isLoading: false
+      };
+    }
+    case GET_ONE_INTERVIEW_REJECTED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    case ADD_INTERVIEW_FETCHING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case ADD_INTERVIEW_FULFILLED: {
+      return {
+        ...state,
+        isLoading: false,
+        list: [...state.list, action.payload]
+      };
+    }
+    case ADD_INTERVIEW_REJECTED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    case UPDATE_INTERVIEW_FETCHING:
+      return {
+        ...state,
+        error: false,
+        isLoading: true
+      };
+    case UPDATE_INTERVIEW_FULFILLED:
+      return {
+        ...state,
+        isLoading: false,
+        list: [...state.list]
+      };
+    case UPDATE_INTERVIEW_REJECTED:
+      return {
+        ...state,
+        isLoading: false,
+        error: true,
+        errorMessage: action.payload
+      };
+
     case DELETE_INTERVIEW_FETCHING:
       return {
         ...state,
