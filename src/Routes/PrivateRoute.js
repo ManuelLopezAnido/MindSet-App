815dcba84ee6c5ceeffca50e6586f4e66c40a823
Joaquin leimeter <leimeter.joaquin@gmail.com>
2022-01-17 19:19:51 -0300

    modifyed private route

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -1,18 +1,7 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { setAuthentication } from 'redux/auth/actions';
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const token = sessionStorage.getItem('token');
-    if (token) {
-      dispatch(setAuthentication());
-    }
-  }, []);
-
   return (
     <Route
       {...rest}
