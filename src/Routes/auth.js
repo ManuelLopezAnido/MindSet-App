237df1b457bbeaa3d042fe9ca7981661e8c73f8a
Joaquin leimeter <leimeter.joaquin@gmail.com>
2022-01-17 17:24:18 -0300

    added auth signUp route

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -2,6 +2,7 @@ import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
 import HomePageLayout from 'Components/Layout/HomePage/HomePageLayout';
 import Login from 'Components/Auth/Login';
 import Register from 'Components/Auth/Register';
+import SignUp from 'Components/Postulant/SignUp';
 
 const HomeRoutes = () => {
   const { url } = useRouteMatch();
@@ -9,7 +10,8 @@ const HomeRoutes = () => {
     <HomePageLayout>
       <Switch>
         <Route path={`${url}/login`} exact component={Login} />
-        <Route path={`${url}/signUp`} exact component={Register} />
+        <Route path={`${url}/register`} exact component={Register} />
+        <Route path={`${url}/signUp`} exact component={SignUp} />
         <Redirect to={`${url}/login`} />
       </Switch>
     </HomePageLayout>
